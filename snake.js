dbf31414faab7298d0670ffb9dba4a8791aaa0d0
BykoButeyko <44642673+BykoButeyko@users.noreply.github.com>
2020-06-16 17:28:30 +0200

    update snake moves
    Snake move fixed.
    Issue: snake swap direction

--- snake.js
+++ snake.js
@@ -1,14 +1,7 @@
-import { getInputDirection } from "./input"
+import { getInputDirection } from "./input.js"
 
 export const snakeSpeed = 1
-const snakeBody = [
-    { x: 10, y: 11},
-    { x: 11, y: 11},
-    { x: 12, y: 11},
-    { x: 13, y: 11},
-    { x: 14, y: 11},
-
-]
+const snakeBody = [{ x: 10, y: 11}]
 
 export function update() {
     const InputDirection = getInputDirection()
