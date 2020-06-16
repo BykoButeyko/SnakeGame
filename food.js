945dcc191b9829e9bd8108c942ac0a4ce2b2a643
BykoButeyko <44642673+BykoButeyko@users.noreply.github.com>
2020-06-16 19:27:44 +0200

    final touches
    final touches in the code
    fix bugs

--- food.js
+++ food.js
@@ -13,8 +13,8 @@ export function update() {
 
 export function draw(gameBoard) {
     const foodElement = document.createElement('div')
-        foodElement.style.gridRowStart = segment.y
-        foodElement.style.gridColumnStart = segment.x
+        foodElement.style.gridRowStart = food.y
+        foodElement.style.gridColumnStart = food.x
         foodElement.classList.add('food')
         gameBoard.appendChild(foodElement)
 }
